608dbeb3acf2f363a15f1856bdd8161a18f21f80
vyzyv <vyz@protonmail.com>
2020-09-10 09:43:58 +0200

    Add Tensor cacher

--- torchdata/cachers.py
+++ torchdata/cachers.py
@@ -19,6 +19,8 @@ import pickle
 import shutil
 import typing
 
+import torch
+
 from ._base import Base
 
 
@@ -100,11 +102,13 @@ class Pickle(Cacher):
     You can also issue `clean()` method manually for the same effect
     (though it's discouraged as you might crash `__setitem__` method).
 
-    **Important:**
 
-    This `cacher` can act between consecutive runs, just don't use `clean()` method
-    or don't delete the folder manually. If so, **please ensure correct sampling**
-    (same seed and sampling order) for reproducible behaviour between runs.
+    .. note::
+
+        This `cacher` can act between consecutive runs, just don't use `clean()` method
+        or don't delete the folder manually. If so, **please ensure correct sampling**
+        (same seed and sampling order) for reproducible behaviour between runs.
+
 
     Attributes
     ----------
@@ -186,3 +190,118 @@ class Memory(Cacher):
     def __getitem__(self, index: int):
         """Retrieve data from dictionary."""
         return self.cache[index]
+
+
+class Tensor(Cacher):
+    r"""**Save and load data from disk using PyTorch's ** `save` **function.**
+
+    Tensors will be saved as `.pt` in specified path. If path does not exist,
+    it will be created.
+
+    **This object can be used as a** `context manager` **and it will delete** `path` **at the end of block**::
+
+        with td.cachers.Tensor(pathlib.Path("./disk")) as cacher:
+            dataset = dataset.map(lambda x: x+1).cache(cacher)
+            ... # Do something with dataset
+        ... # Folder removed
+
+    You can also issue `clean()` method manually for the same effect
+    (though it's discouraged as you might crash `__setitem__` method).
+
+
+    .. note::
+
+        This `cacher` can act between consecutive runs, just don't use `clean()` method
+        or don't delete the folder manually. If so, **please ensure correct sampling**
+        (same seed and sampling order) for reproducible behaviour between runs.
+
+    Attributes
+    ----------
+    path: pathlib.Path
+        Path to the folder where samples will be saved and loaded from.
+    extension: str
+        Extension to use for saved pickle files. Default: `.pt`
+    map_location: a function, :class:`torch.device`, string, dict, optional
+        Specify how to remap storage locations. See `torch.load`. Default: `None`
+    pickle_module: module, optional
+        Module used for unpickling metadata and objects (has to
+        match the :attr:`pickle_module` used to serialize file).
+        Default: `pickle`
+    pickle_protocol: int, optional
+        Can be specified to override the default protocol. See `torch.save`.
+        Default: `2` (`pickle` default)
+    **pickle_load_args
+        optional keyword arguments passed over to :func:`pickle_module.load`
+        and :func:`pickle_module.Unpickler`, e.g., :attr:`errors=...`.
+        See `torch.load`
+
+    """
+
+    def __init__(
+        self,
+        path: pathlib.Path,
+        extension: str = ".pt",
+        map_location=None,
+        pickle_module=pickle,
+        pickle_protocol=2,
+        **pickle_load_args
+    ):
+        self.path = path
+        self.path.mkdir(parents=True, exist_ok=True)
+        self.extension = extension
+        self.map_location = map_location
+        self.pickle_module = pickle_module
+        self.pickle_protocol = pickle_protocol
+        self.pickle_load_args = pickle_load_args
+
+    def __contains__(self, index: int) -> bool:
+        """**Check whether file exists on disk.**
+
+        If file is available it is considered cached, hence you can cache data
+        between multiple runs (if you ensure repeatable sampling).
+
+        """
+        return pathlib.Path(
+            (self.path / str(index)).with_suffix(self.extension)
+        ).is_file()
+
+    def __setitem__(self, index: int, data: int):
+        """**Save** `data` **in specified folder.**
+
+        Name of the item will be equal to `{self.path}/{index}{extension}`.
+
+        """
+        torch.save(
+            data,
+            (self.path / str(index)).with_suffix(self.extension),
+            pickle_module=self.pickle_module,
+            pickle_protocol=self.pickle_protocol,
+        )
+
+    def __getitem__(self, index: int):
+        """**Retrieve** `data` **specified by** `index`.
+
+        Name of the item will be equal to `{self.path}/{index}{extension}`.
+
+        """
+        torch.load(
+            (self.path / str(index)).with_suffix(self.extension),
+            map_location=self.map_location,
+            pickle_module=self.pickle_module,
+            **self.pickle_load_args
+        )
+
+    def clean(self) -> None:
+        """**Remove recursively folder** `self.path`.
+
+        Behaves just like `shutil.rmtree`, but won't act if directory does not exist.
+        """
+
+        if self.path.is_dir():
+            shutil.rmtree(self.path)
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, *args):
+        self.clean()
